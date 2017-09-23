78e03304567a7825258a1c55355fa5ec8566ac32
Annie Mathis <annie_mathis@baylor.edu>
2017-09-22 23:28:01 -0500

    all the seats are inserted in SeatList

--- client/thesis2.js
+++ client/thesis2.js
@@ -8,12 +8,37 @@ import { Meteor } from 'meteor/meteor';
 import './thesis2.html';
 
 setActive = function(){
+    console.log("hi");
+}
+
+if(Meteor.isClient){
+    Meteor.subscribe('seats');
+    Meteor.subscribe('questions');
+    Meteor.subscribe('log');
+
 
 }
 
+
 if(Meteor.isServer){
-    Meteor.onConnection(function(conn) {
-        console.log(conn.clientAddress);
-        Session.set('ip', conn.clientAddress);
+    Meteor.publish('seats', function(){
+        return SeatList.find();
     });
+    Meteor.publish('questions', function(){
+        return Questions.find();
+    });
+    Meteor.publish('log', function(){
+        return Log.find();
+    });  
+
+    // Listen to incoming HTTP requests, can only be used on the server
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return next();
+  });
+
+  Meteor.onConnection(function(conn) {
+    console.log(conn.clientAddress);
+    Session.set('ip', conn.clientAddress);
+});
 }
